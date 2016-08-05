f4b5eafbb6f4cafb9f1dec53685864d56c0518f2
Xaver Loppenstedt <xaver@loppenstedt.de>
2016-08-05 12:54:20 +0200

    Support Ubuntu unattended ODBC package install

--- ODBC install scripts/installodbc_ubuntu.sh
+++ ODBC install scripts/installodbc_ubuntu.sh
@@ -5,7 +5,7 @@ sudo wget ftp://ftp.unixodbc.org/pub/unixODBC/unixODBC-2.3.1.tar.gz -P /tmp/msod
 cd /tmp/msodbcubuntu
 sudo tar -xzf /tmp/msodbcubuntu/unixODBC-2.3.1.tar.gz 
 cd /tmp/msodbcubuntu/unixODBC-2.3.1/ 
-sudo apt-get install g++-5
+sudo apt-get -y install g++-5
 echo "Configuring the unixODBC 2.3.1 Driver Manager"
 ./configure --disable-gui --disable-drivers --enable-iconv --with-iconv-char-enc=UTF8 --with-iconv-ucode-enc=UTF16LE 1> odbc_con.log 2> moake_err.log
 echo "Building and Installing the unixODBC 2.3.1 Driver Manager"
@@ -19,8 +19,8 @@ tar xvfz /tmp/msodbcubuntu/msodbcsql-13.0.0.0.tar.gz
 cd /tmp/msodbcubuntu/msodbcsql-13.0.0.0/
 ldd /tmp/msodbcubuntu/msodbcsql-13.0.0.0/lib64/libmsodbcsql-13.0.so.0.0
 echo "Installing Dependencies"
-sudo apt-get install libssl1.0.0 
-sudo apt-get install libgss3 
+sudo apt-get -y install libssl1.0.0
+sudo apt-get -y install libgss3
 sudo ldconfig
 echo "Installing the Microsoft ODBC Driver 13 for SQL Server- Ubuntu"
 sudo bash ./install.sh install --force --accept-license
