f40832fb6b0006d407a3df985d40b96ee4715e69
david-puglielli <davidp@simba.com>
2016-08-12 15:35:16 -0700

    Update script to remove failing command
    cd usr/lib64 does nothing and appears to be extraneous, so I have removed it entirely. At this point in the script we have already cd'ed into the required directory.

--- ODBC install scripts/installodbc_redhat.sh
+++ ODBC install scripts/installodbc_redhat.sh
@@ -53,7 +53,6 @@ sudo ln -s libodbc.so.2     libodbc.so.1
 
 cd /tmp/msodbcrhel/
 
-cd usr/lib64
 if sudo wget https://download.microsoft.com/download/B/C/D/BCDD264C-7517-4B7D-8159-C99FC5535680/msodbcsql-13.0.0.0.tar.gz ; 
 then 
 	echo "Successfuly download the ODBC Driver and tools." 
