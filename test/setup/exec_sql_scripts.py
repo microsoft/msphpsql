15fa9d1b7b6b9e434ce56b209fb59de9c84c7f48
yitam <v-yitam@microsoft.com>
2017-05-02 14:39:55 -0700

    fixed database variable

--- test/setup/exec_sql_scripts.py
+++ test/setup/exec_sql_scripts.py
@@ -28,11 +28,11 @@ def executeSQLscriptUnix(sqlfile, conn_options, dbname):
     # This is a workaround because sqlcmd in Unix does not support -v option for variables. 
     # It inserts setvar dbname into the beginning of a temp .sql file
     tmpFileName = sqlfile[0:-4] + '_tmp.sql'
-    redirect_string = '(echo :setvar dbname {0})  > {2}; cat {1} >> {2}; '
+    redirect_string = '(echo :setvar dbname {0})  > {2}; cat {1} >> {2}; cat {2}; '
     sqlcmd = 'sqlcmd ' + conn_options + ' -i ' + tmpFileName
 
     print ('display contents of tmpFileName')
-    show_cmd = 'cat ' + tmpFileName
+    show_cmd = 'sqlcmd ' + conn_options + ' -Q select @@Version '
     executeCommmand(show_cmd)
 
     inst_command = redirect_string.format(dbname, sqlfile, tmpFileName) + sqlcmd
