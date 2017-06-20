a879e4016298f97baaada09f29f9051894ac7493
Hadis Kakanejadi Fard <v-hakaka@microsoft.com>
2017-06-20 10:12:23 -0700

    updated version to 4.3 rc

--- source/pdo_sqlsrv/pdo_dbh.cpp
+++ source/pdo_sqlsrv/pdo_dbh.cpp
@@ -3,7 +3,7 @@
 //
 // Contents: Implements the PDO object for PDO_SQLSRV
 //
-// Microsoft Drivers 4.2 for PHP for SQL Server
+// Microsoft Drivers 4.3 for PHP for SQL Server
 // Copyright(c) Microsoft Corporation
 // All rights reserved.
 // MIT License
