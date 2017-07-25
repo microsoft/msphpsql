5a3f2ba2010035d146710d9811c966cbab6e0de5
David Puglielli <v-dapugl@microsoft.com>
2017-07-25 11:44:01 -0700

    Update version.h
    Fixed the location of the dash, since pecl does not like dashes it gets included in VER_FILEVERSION_STR instead of SEMVER_PRERELEASE.

--- source/shared/version.h
+++ source/shared/version.h
@@ -35,7 +35,7 @@
 // for stable releases should be empty
 // "-RC" for release candidates
 // "-preview" for ETP 
-#define SEMVER_PRERELEASE "-preview"
+#define SEMVER_PRERELEASE "preview"
 // Semantic versioning build metadata, build meta data is not counted in precedence order.
 #define SEMVER_BUILDMETA
 
@@ -47,8 +47,10 @@
 // Main version, dot separated 3 digits, Major.Minor.Patch
 #define VER_APIVERSION_STR      STRINGIFY( SQLVERSION_MAJOR ) "." STRINGIFY( SQLVERSION_MINOR ) "." STRINGIFY( SQLVERSION_PATCH )
 
-// Remove "-" if SEMVER_PRERELEASE is empty (for stable releases)
-#define VER_FILEVERSION_STR     VER_APIVERSION_STR SEMVER_PRERELEASE SEMVER_BUILDMETA
+// For preview release, we want the following:
+// #define VER_FILEVERSION_STR     VER_APIVERSION_STR "-" SEMVER_PRERELEASE SEMVER_BUILDMETA
+// because pecl doesn't like dashes. However, if SEMVER_PRERELEASE is empty, the "-" must be removed
+#define VER_FILEVERSION_STR     VER_APIVERSION_STR "-" SEMVER_PRERELEASE SEMVER_BUILDMETA
 #define _FILEVERSION            SQLVERSION_MAJOR,SQLVERSION_MINOR,SQLVERSION_PATCH,SQLVERSION_BUILD
 
 // PECL package version macros (can't have '-' or '+')
