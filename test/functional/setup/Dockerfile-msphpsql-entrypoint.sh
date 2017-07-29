a3fa58a69928ca31894e1c53ebab4d896f46bbb0
Hadis-Fard <v-hakaka@microsoft.com>
2017-07-28 17:20:20 -0700

    fixed qoutes

--- test/functional/setup/Dockerfile-msphpsql-entrypoint.sh
+++ test/functional/setup/Dockerfile-msphpsql-entrypoint.sh
@@ -1,6 +1,6 @@
 set -e
 
-db_migrate="/opt/mssql-tools/bin/sqlcmd -S sql -U sa -P Password123  -Q "select @@Version"
+db_migrate='/opt/mssql-tools/bin/sqlcmd -S sql -U sa -P Password123  -Q "select @@Version"'
 
 until $db_migrate; do
 >&2 echo "SQL Server is starting up. Running initial db configuration"
