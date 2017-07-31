897468626f4dc7a53816ae899c511414bd214424
Hadis-Fard <v-hakaka@microsoft.com>
2017-07-31 16:03:50 -0700

    added test for connection from client to server

---
+++ TestSqlcmd.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+set -e
+
+$test = "sqlcmd  -S sql -U sa -P Password123"
+
+until $test; do
+>&2 echo "SQL Server is starting up. Running initial db configuration"
+sleep 1
+done
+
+echo "SQL Server is up - starting app"
