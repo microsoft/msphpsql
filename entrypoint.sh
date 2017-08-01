98753f0998be86d7a626f9f1d8c73a0138cf4588
Hadis-Fard <v-hakaka@microsoft.com>
2017-08-01 15:51:12 -0700

    updated entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 set -e
+isConnected="/opt/mssql-tools/bin/sqlcmd -S db -U sa -P Password123"
 
-db_migrate='/opt/mssql-tools/bin/sqlcmd -S sql -U sa -P Password123  -Q "select @@Version"'
-
-until $db_migrate; do
+until $isConnected; do
 >&2 echo "SQL Server is starting up. Running initial db configuration"
 sleep 1
 done
