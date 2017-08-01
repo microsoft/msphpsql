c3b305e42c4fdf629d2f1c6b11fe8e3fa9bad147
Hadis-Fard <v-hakaka@microsoft.com>
2017-07-31 17:44:11 -0700

    removed extra space

--- TestSqlcmd.sh
+++ TestSqlcmd.sh
@@ -1,9 +1,9 @@
 set -e
 
-$test = "/opt/mssql-tools/bin/sqlcmd  -S sql -U sa -P Password123"
+$test ='/opt/mssql-tools/bin/sqlcmd -S sql -U sa -P Password123'
 
 until $test; do
->&2 echo "SQL Server is starting up. Running initial db configuration"
+>&2 echo "SQL Server is starting up"
 sleep 1
 done
 
