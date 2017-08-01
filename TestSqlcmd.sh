8983799f38875ced03357207d7622b140df9b017
Hadis-Fard <v-hakaka@microsoft.com>
2017-07-31 18:06:47 -0700

    just exec sleep

--- TestSqlcmd.sh
+++ TestSqlcmd.sh
@@ -1,10 +1,1 @@
-set -e
-
-$test ="/opt/mssql-tools/bin/sqlcmd -S sql -U sa -P Password123"
-
-until $test; do
->&2 echo "SQL Server is starting up"
-sleep 1
-done
-
-echo "SQL Server is up - starting app"
+sleep 1m
