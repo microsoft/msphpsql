b7f7708817cabf76bb3ff10aa0fcb72b35bfe76f
Hadis-Fard <v-hakaka@microsoft.com>
2017-08-02 12:38:18 -0700

    added ping to the entrypoint too

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,11 @@
 set -e
-isConnected="/opt/mssql-tools/bin/sqlcmd -S $1 -U $2 -P $3"
 
-until $isConnected; do
->&2 echo "SQL Server is starting up..."
+while ! ping -c3 $1 &>/dev/null; do echo "Ping Fail - `date`"; done ; echo "Host Found - `date`";
+
+testConnection="/opt/mssql-tools/bin/sqlcmd -S $1 -U $2 -P $3"
+
+until $testConnection; do
+>&2 echo "SQL Server is starting up.."
 sleep 1
 done
 
