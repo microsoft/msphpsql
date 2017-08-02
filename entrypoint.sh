651b72cef208ea5d30d7404e4508b75dd271f724
Hadis-Fard <v-hakaka@microsoft.com>
2017-08-02 12:47:39 -0700

    removed ping

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 set -e
 
-while ! ping -c3 $1 &>/dev/null; do echo "Ping Fail - `date`"; done ; echo "Host Found - `date`";
-
-testConnection="/opt/mssql-tools/bin/sqlcmd -S $1 -U $2 -P $3"
+testConnection="/opt/mssql-tools/bin/sqlcmd -S sql -U sa -P Pasword123"
 
 until $testConnection; do
 >&2 echo "SQL Server is starting up.."
