101eaf2c66e11dda701a1a21d0b5acf09b2399ed
Hadis-Fard <v-hakaka@microsoft.com>
2017-08-02 09:46:59 -0700

    passing arg to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 set -e
-isConnected="/opt/mssql-tools/bin/sqlcmd -S db -U sa -P Password123"
+isConnected="/opt/mssql-tools/bin/sqlcmd -S $1 -U $2 -P $3"
 
 until $isConnected; do
->&2 echo "SQL Server is starting up. Running initial db configuration"
+>&2 echo "SQL Server is starting up..."
 sleep 1
 done
 
->&2 echo "SQL Server is up - starting app"
+>&2 echo "SQL Server is up!"
