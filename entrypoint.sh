7c305fadbceb496357a066f971cf943138c17ab6
Hadis-Fard <v-hakaka@microsoft.com>
2017-08-03 13:47:56 -0700

    limited the loop to 10 times, max 60 sec wait

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,15 @@ set -e
 
 testConnection="/opt/mssql-tools/bin/sqlcmd -S sql -U sa -P Password123"
 
-until $testConnection; do
+for run in {1..10}; do
+
 >&2 echo "SQL Server is starting up.."
-sleep 1
+if $testConnection; then
+    >&2 echo "SQL Server is up!"
+    break;
+else
+    sleep 6
+fi
 done
 
->&2 echo "SQL Server is up!"
+
