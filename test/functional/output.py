ee511175af6df7494ceeaedd47022f74e28440b6
Jenny Tam <v-yitam@microsoft.com>
2017-11-20 10:54:05 -0800

    Overhauled output.py to make it read the log file only once

--- test/functional/output.py
+++ test/functional/output.py
@@ -2,10 +2,10 @@
 #########################################################################################
 #
 # Description:
-#       Requirement of python 3.4 to execute this script and required result log file are in the same location
+#       Requirement of python 3.4 to execute this script and required result log file(s) 
+#       are in the same location
 #       Run with command line without options required. Example: py output.py
 #       This script parse output of PHP Native Test
-#
 #
 #############################################################################################
 
@@ -13,128 +13,82 @@ import os
 import stat
 import re
 
-# This module returns either the number of test or the number of failed test
-# depending on the argument you requested.
-# Input:    var - a single variable containing either "FAIL" or "TOTAL"
-# Output:   Returns a number of test/s or failed test/s
-def returnCount(var):
-    with open(os.path.dirname(os.path.realpath(__file__)) + os.sep + logfile) as f:
-        num = 0
-        failnum = 0
-        for line in f:
-            if "FAIL" in line or "PASS" in line:
-                if ".phpt" in line:
-                    if "FAIL" in line:
-                        failnum += 1
-                    num += 1
-    if var == 'total':
-        return str(num)
+# This module appends an entry to the tests list, may include the test title.
+# Input:    search_pattern - pattern to look for in the line of the log file
+#           line - current line of the log file
+#           index - the current index of tests
+#           tests_list - a list of xml entries
+#           get_title - boolean flag to get the test title or not
+# Output:   None
+def get_test_entry(search_pattern, line, index, tests_list, get_title = False):
+    # find the full path to the test name, enclosed by square brackets
+    result = re.search(search_pattern, line)
+    pos1 = result.group(1).find('[')
+    pos2 = result.group(1).find(']')
+    test_line = str(result.group(1))
+
+    # get the test name by splitting this full path delimited by os.sep
+    substr = test_line[pos1+1:pos2]
+    tmp_array = substr.split(os.sep)
+    pos = len(tmp_array) - 1
+    test_name = tmp_array[pos]
+
+    # only upon a failure do we get the test title
+    if (get_title is True):
+        entry = '\t<testcase name="' + test_name + '-' + index + '">'
+        tests_list.append(entry)
+        test_title = test_line[0:pos1]
+        entry = '\t\t<failure message=" Failed in ' + test_title + '"/>'
+        tests_list.append(entry)
+        tests_list.append('\t</testcase>')
     else:
-        return str(failnum)
+        entry = '\t<testcase name="' + test_name + '-' + index + '"/>'
+        tests_list.append(entry)
 
-# This module prints the line that matches the expression.
-# Input:    inputStr - String that matches
-#           file - file name
-#           path - path of the file.
-# Output:   null
-def readAndPrint(inputStr, file, path):
-    print(path)
-    filn = open(path + os.sep + file).readlines()
-    for lines in filn:
-        if inputStr in lines:
-            print(lines)
+# Extract individual test results from the log file and
+# enter it in the nativeresult.xml file.
+# Input:    logfile - the log file
+#           number - the number for this xml file
+def gen_XML(logfile, number):
+    print('================================================')
+    print("\n" + os.path.splitext(logfile)[0] + "\n" )
 
-# This module returns the test file name.
-# Input:    line - current line of the log file
-# Output:   Returns the filename.
-def TestFilename(line):
-    terminateChar = os.sep
-    currentPos = 0
-    firstpos = len(line) * -1
-    print(line)
-    print(firstpos)
-    while True:
-        currentPos = currentPos - 1
-        # if passed the first pos, stop
-        if currentPos < firstpos: 
-            break
-        line[currentPos]
-        if line[currentPos] == terminateChar:
-            break         
-    file = line[currentPos+1:-1]
-    return file
-
-def genXML(logfile,number):
-    # Generating the nativeresult.xml file.
-    file = open('nativeresult' + str(number) + '.xml','w')
-    file.write('<?xml version="1.0" encoding="UTF-8" ?>' + os.linesep)
-    file.write('<testsuite tests="' + returnCount('total') + '" failures="' + returnCount('fail') + '" name="Native Tests" >' + os.linesep)
-    file.close()
-
-    # Extract individual test results from the log file and
-    # enter it in the nativeresult.xml file.
-
+    tests_list = []
     with open(os.path.dirname(os.path.realpath(__file__)) + os.sep + logfile) as f:
         num = 1
         failnum = 0
         for line in f:
-            file = open('nativeresult' + str(number) + '.xml','a')
             if "FAIL" in line or "PASS" in line:
                 if ".phpt" in line:
-
-                    file.write('\t<testcase name="')
                     if "FAIL" in line:
                         failnum += 1
-                        result = re.search('FAIL(.*).', line)
-                        file.write(TestFilename(str(result.group(1))) + '-' + str(num) + '">' + os.linesep)
-                        stop_pos = result.group(1).find('[')
-                        file.write('\t\t<failure message=" Failed in ' + str(result.group(1))[0:stop_pos] + '"/>' + os.linesep)
-                        file.write('\t</testcase>' + os.linesep)
+                        get_test_entry('FAIL(.*).', line, str(num), tests_list, True)
                     else:
-                        result = re.search('PASS(.*).', line)
-                        file.write(TestFilename(str(result.group(1))) + '-' + str(num) + '"/>' + os.linesep)
+                        get_test_entry('PASS(.*).', line, str(num), tests_list)
                     num += 1
-            file.close()
+            elif 'Number of tests :' in line or 'Tests skipped ' in line or 'Tests warned ' in line or'Tests failed ' in line or 'Expected fail ' in line or 'Tests passed ' in line:
+                print(line)
+        print('================================================')
 
-        file = open('nativeresult' + str(number) + '.xml','a')
-        file.write('</testsuite>' + os.linesep)
-        file.close()
+    # Generating the nativeresult.xml file.
+    file = open('nativeresult' + str(number) + '.xml', 'w')
+    file.write('<?xml version="1.0" encoding="UTF-8" ?>' + os.linesep)
+    file.write('<testsuite tests="' + str(num - 1) + '" failures="' + str(failnum) + '" name="Native Tests" >' + os.linesep)
 
-def run():
-    num = 1
-    for f in os.listdir(os.path.dirname(os.path.realpath(__file__))):
-        if f.endswith("log"):
-            print('================================================')
-            print(os.path.splitext(f)[0])
-            readAndPrint('Number of tests :', f, os.path.dirname(os.path.realpath(__file__)))
-            readAndPrint('Tests skipped ', f, os.path.dirname(os.path.realpath(__file__)))
-            readAndPrint('Tests warned ', f, os.path.dirname(os.path.realpath(__file__)))
-            readAndPrint('Tests failed ', f, os.path.dirname(os.path.realpath(__file__)))
-            readAndPrint('Expected fail ', f, os.path.dirname(os.path.realpath(__file__)))
-            readAndPrint('Tests passed ', f, os.path.dirname(os.path.realpath(__file__)))
-            print('================================================')
-            logfile = f
-            genXML(logfile,num)
-            num = num + 1
+    index = 1
+    for test in tests_list:
+        file.write(test + os.linesep)
+    file.write('</testsuite>' + os.linesep)
+    file.close()
 
-
-# ------------------------------------------------------- Main Function ---------------------------------------------------
+# ----------------------- Main Function -----------------------
 
 # Display results on screen from result log file.
 if __name__ == '__main__':
     num = 1
     for f in os.listdir(os.path.dirname(os.path.realpath(__file__))):
         if f.endswith("log"):
-            print('================================================')
-            print("\n" + os.path.splitext(f)[0] + "\n")
-            readAndPrint('Number of tests :', f, os.path.dirname(os.path.realpath(__file__)))
-            readAndPrint('Tests skipped ', f, os.path.dirname(os.path.realpath(__file__)))
-            readAndPrint('Tests warned ', f, os.path.dirname(os.path.realpath(__file__)))
-            readAndPrint('Tests failed ', f, os.path.dirname(os.path.realpath(__file__)))
-            readAndPrint('Expected fail ', f, os.path.dirname(os.path.realpath(__file__)))
-            readAndPrint('Tests passed ', f, os.path.dirname(os.path.realpath(__file__)))
-            print('================================================')
             logfile = f
-            genXML(logfile,num)
+            gen_XML(logfile, num)
             num = num + 1
 
