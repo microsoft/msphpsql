c445b47997b7a78ab50e2abdd7a3a9a42f2650db
Jenny Tam <v-yitam@microsoft.com>
2018-08-20 14:15:19 -0700

    Incorporated changes in PR 634 to pdo_sqlsrv

--- source/pdo_sqlsrv/pdo_stmt.cpp
+++ source/pdo_sqlsrv/pdo_stmt.cpp
@@ -560,12 +560,15 @@ int pdo_sqlsrv_stmt_execute( _Inout_ pdo_stmt_t *stmt TSRMLS_DC )
 
         // if the user is using prepare emulation (PDO::ATTR_EMULATE_PREPARES), set the query to the 
         // subtituted query provided by PDO
-        if( stmt->supports_placeholders == PDO_PLACEHOLDER_NONE ) {
+        if (stmt->supports_placeholders == PDO_PLACEHOLDER_NONE) {
             // reset the placeholders hashtable internal in case the user reexecutes a statement
-            zend_hash_internal_pointer_reset(driver_stmt->placeholders);
+            // do not directly alter the internal pointer in the array (See pull request 634 on GitHub)
+            HashPosition pos;
+
+            zend_hash_internal_pointer_reset_ex(driver_stmt->placeholders, &pos);
 
             query = stmt->active_query_string;
-            query_len = static_cast<unsigned int>( stmt->active_query_stringlen );
+            query_len = static_cast<unsigned int>(stmt->active_query_stringlen);
         }
 
         SQLRETURN execReturn = core_sqlsrv_execute( driver_stmt TSRMLS_CC, query, query_len );
