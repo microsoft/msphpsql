9204f3aa3dc22b345dc07e190df757a972eafcf5
Erik Campobadal <soc@erik.cat>
2018-10-27 13:07:01 +0200

    Fixed line endings and identation

--- sample/pdo_sqlsrv_sample.php
+++ sample/pdo_sqlsrv_sample.php
@@ -1,78 +1,82 @@
 <?php
-    echo "\n";
-    $serverName = 'tcp:yourserver.database.windows.net,1433';
-	$database = 'yourdatabase';
-	$uid = 'yourusername';
-	$pwd = 'yourpassword';
 
-	//Establishes the connection
-	$conn = new PDO("sqlsrv:server={$serverName}; Database = {$database}", $uid, $pwd);
+echo "\n";
+$serverName = 'tcp:yourserver.database.windows.net,1433';
+$database = 'yourdatabase';
+$uid = 'yourusername';
+$pwd = 'yourpassword';
 
-	//Select Query
-	$tsql = 'SELECT [CompanyName] FROM SalesLT.Customer';
+//Establishes the connection
+$conn = new PDO("sqlsrv:server={$serverName}; Database = {$database}", $uid, $pwd);
 
-	//Executes the query
-	$getProducts = $conn->query($tsql);
+//Select Query
+$tsql = 'SELECT [CompanyName] FROM SalesLT.Customer';
 
-	//Error handling
-	FormatErrors($conn->errorInfo());
+//Executes the query
+$getProducts = $conn->query($tsql);
 
-	$productCount = 0;
-	$ctr = 0;
+//Error handling
+FormatErrors($conn->errorInfo());
+
+$productCount = 0;
+$ctr = 0;
 
 ?>
 
 <h1> First 10 results are : </h1>
 
 <?php
-	while($row = $getProducts->fetch(PDO::FETCH_ASSOC)) {
-		if ($ctr > 9) {
-			break;
-		}
+while($row = $getProducts->fetch(PDO::FETCH_ASSOC)) {
+    if ($ctr > 9) {
+        break;
+    }
 
-		$ctr++;
+    $ctr++;
 
-		echo "{$row['CompanyName']}<br/>";
+    echo "{$row['CompanyName']}<br/>";
 
-		$productCount++;
-	}
+    $productCount++;
+}
 
-	$getProducts = NULL;
+$getProducts = NULL;
 
-	$tsql = "INSERT INTO SalesLT.Product (Name, ProductNumber, StandardCost, ListPrice, SellStartDate) OUTPUT INSERTED.* VALUES ('SQL New 1', 'SQL New 2', 0, 0, getdate())";
+$tsql = "INSERT INTO SalesLT.Product (Name, ProductNumber, StandardCost, ListPrice, SellStartDate) OUTPUT INSERTED.* VALUES ('SQL New 1', 'SQL New 2', 0, 0, getdate())";
 
-	//Insert query
-	$insertReview = $conn->query($tsql);
-	FormatErrors($conn->errorInfo());
+//Insert query
+$insertReview = $conn->query($tsql);
+FormatErrors($conn->errorInfo());
+
 ?>
 
 <h1> Product Key inserted is :</h1>
 
 <?php
-	while ($row = $insertReview->fetch(PDO::FETCH_ASSOC)) {
-		echo "{$row['ProductID']}<br/>";
-	}
 
-	$insertReview = null;
+while ($row = $insertReview->fetch(PDO::FETCH_ASSOC)) {
+    echo "{$row['ProductID']}<br/>";
+}
 
-	//Delete Query
-	//We are deleting the same record
-	$tsql = 'DELETE FROM [SalesLT].[Product] WHERE Name=?';
-	$param = 'SQL New 1';
+$insertReview = null;
 
-	$deleteReview = $conn->prepare($tsql);
-	$deleteReview->bindParam(1, $param);
+//Delete Query
+//We are deleting the same record
+$tsql = 'DELETE FROM [SalesLT].[Product] WHERE Name=?';
+$param = 'SQL New 1';
 
-	$deleteReview->execute();
-	FormatErrors($deleteReview->errorInfo());
+$deleteReview = $conn->prepare($tsql);
+$deleteReview->bindParam(1, $param);
 
-	function FormatErrors($error)
-	{
-		/* Display error. */
-		echo "Error information: <br/>";
+$deleteReview->execute();
+FormatErrors($deleteReview->errorInfo());
 
-		echo "SQLSTATE: {$error[0]}<br/>";
-		echo "Code: {$error[1]}<br/>";
-		echo "Message: {$error[2]}<br/>";
-	}
+function FormatErrors($error)
+{
+    /* Display error. */
+    echo "Error information: <br/>";
+
+    echo "SQLSTATE: {$error[0]}<br/>";
+    echo "Code: {$error[1]}<br/>";
+    echo "Message: {$error[2]}<br/>";
+}
+
 ?>
--- sample/sqlsrv_sample.php
+++ sample/sqlsrv_sample.php
@@ -1,89 +1,97 @@
 <?php
-    echo "\n";
-    $serverName = 'tcp:yourserver.database.windows.net,1433';
-    $connectionOptions = [
-		'Database' => 'yourdatabase',
-		'Uid' => 'yourusername',
-		'PWD' => 'yourpassword'
-	];
 
-	 //Establishes the connection
-	 $conn = sqlsrv_connect($serverName, $connectionOptions);
+echo "\n";
+$serverName = 'tcp:yourserver.database.windows.net,1433';
+$connectionOptions = [
+    'Database' => 'yourdatabase',
+    'Uid' => 'yourusername',
+    'PWD' => 'yourpassword'
+];
 
-	 //Select Query
-	 $tsql = 'SELECT [CompanyName] FROM SalesLT.Customer';
+//Establishes the connection
+$conn = sqlsrv_connect($serverName, $connectionOptions);
 
-	 //Executes the query
-	 $getProducts = sqlsrv_query($conn, $tsql);
+//Select Query
+$tsql = 'SELECT [CompanyName] FROM SalesLT.Customer';
 
-	 //Error handling
-	 if ($getProducts == false) {
-		die(FormatErrors(sqlsrv_errors()));
-	 }
+//Executes the query
+$getProducts = sqlsrv_query($conn, $tsql);
 
-	 $productCount = 0;
-	 $ctr = 0;
+//Error handling
+if ($getProducts == false) {
+    die(FormatErrors(sqlsrv_errors()));
+}
+
+$productCount = 0;
+$ctr = 0;
 
 ?>
+
 <h1> First 10 results are : </h1>
+
 <?php
-	while ($row = sqlsrv_fetch_array($getProducts, SQLSRV_FETCH_ASSOC)) {
-		if ($ctr > 9) {
-			break;
-		}
 
-		$ctr++;
+while ($row = sqlsrv_fetch_array($getProducts, SQLSRV_FETCH_ASSOC)) {
+    if ($ctr > 9) {
+        break;
+    }
 
-		echo $row['CompanyName'];
-		echo "<br/>";
+    $ctr++;
 
-		$productCount++;
-	}
+    echo $row['CompanyName'];
+    echo "<br/>";
 
-	sqlsrv_free_stmt($getProducts);
+    $productCount++;
+}
 
-	$tsql = "INSERT INTO SalesLT.Product (Name, ProductNumber, StandardCost, ListPrice, SellStartDate) OUTPUT INSERTED.ProductID VALUES ('SQL New 1', 'SQL New 2', 0, 0, getdate())";
+sqlsrv_free_stmt($getProducts);
 
-	//Insert query
-	$insertReview = sqlsrv_query($conn, $tsql);
+$tsql = "INSERT INTO SalesLT.Product (Name, ProductNumber, StandardCost, ListPrice, SellStartDate) OUTPUT INSERTED.ProductID VALUES ('SQL New 1', 'SQL New 2', 0, 0, getdate())";
 
-	if ($insertReview == false) {
-		die(FormatErrors( sqlsrv_errors()));
-	}
+//Insert query
+$insertReview = sqlsrv_query($conn, $tsql);
+
+if ($insertReview == false) {
+    die(FormatErrors( sqlsrv_errors()));
+}
+
 ?>
+
 <h1> Product Key inserted is :</h1>
+
 <?php
-	while ($row = sqlsrv_fetch_array($insertReview, SQLSRV_FETCH_ASSOC)) {
-		echo $row['ProductID'];
-	}
 
-	sqlsrv_free_stmt($insertReview);
+while ($row = sqlsrv_fetch_array($insertReview, SQLSRV_FETCH_ASSOC)) {
+    echo $row['ProductID'];
+}
 
-	//Delete Query
-	//We are deleting the same record
-	$tsql = 'DELETE FROM [SalesLT].[Product] WHERE Name=?';
-	$params = ['SQL New 1'];
+sqlsrv_free_stmt($insertReview);
 
-	$deleteReview = sqlsrv_prepare($conn, $tsql, $params);
+//Delete Query
+//We are deleting the same record
+$tsql = 'DELETE FROM [SalesLT].[Product] WHERE Name=?';
+$params = ['SQL New 1'];
 
-	if ($deleteReview == false) {
-		die(FormatErrors(sqlsrv_errors()));
-	}
+$deleteReview = sqlsrv_prepare($conn, $tsql, $params);
 
-	if (sqlsrv_execute($deleteReview) == false) {
-		die(FormatErrors(sqlsrv_errors()));
-	}
+if ($deleteReview == false) {
+    die(FormatErrors(sqlsrv_errors()));
+}
 
-	function FormatErrors($errors)
-	{
-	    /* Display errors. */
-	    echo "Error information: <br/>";
+if (sqlsrv_execute($deleteReview) == false) {
+    die(FormatErrors(sqlsrv_errors()));
+}
 
-	    foreach ($errors as $error) {
-	        echo "SQLSTATE: {$error['SQLSTATE']}<br/>";
-	        echo "Code: {$error['code']}<br/>";
-	        echo "Message: {$error['message']}";
-	    }
-	}
+function FormatErrors($errors)
+{
+    /* Display errors. */
+    echo "Error information: <br/>";
+
+    foreach ($errors as $error) {
+        echo "SQLSTATE: {$error['SQLSTATE']}<br/>";
+        echo "Code: {$error['code']}<br/>";
+        echo "Message: {$error['message']}";
+    }
+}
 
 ?>
