80d90b764212da2f0c609eccd81a6fc0625ce045
Jenny Tam <v-yitam@microsoft.com>
2019-05-08 09:49:38 -0700

    Moved the order

--- buildscripts/buildtools.py
+++ buildscripts/buildtools.py
@@ -270,7 +270,7 @@ class BuildUtil(object):
 
     def generate_build_options(self):
         """Return the generated build configuration and arguments"""
-        cmd_line = '--enable-json'
+        cmd_line = ''
         if self.debug_enabled:
             cmd_line = ' --enable-debug '
             
@@ -282,7 +282,7 @@ class BuildUtil(object):
             else:       # pdo_sqlsrv
                 cmd_line = ' --enable-pdo --with-pdo-sqlsrv=shared ' + cmd_line
                 
-        cmd_line = 'cscript configure.js --disable-all --enable-cli --enable-cgi --enable-embed' + cmd_line
+        cmd_line = 'cscript configure.js --disable-all --enable-cli --enable-cgi --enable-json --enable-embed' + cmd_line
         if self.thread == 'nts':
             cmd_line = cmd_line + ' --disable-zts'
         return cmd_line
