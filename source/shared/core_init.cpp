2792ece85d4cca238ba4b83eecb5286506412ff4
Jenny Tam <v-yitam@microsoft.com>
2019-09-03 12:43:10 -0700

    Updated version to 5.7.0 preview (#1029)

--- source/shared/core_init.cpp
+++ source/shared/core_init.cpp
@@ -3,7 +3,7 @@
 //
 // Contents: common initialization routines shared by PDO and sqlsrv
 //
-// Microsoft Drivers 5.6 for PHP for SQL Server
+// Microsoft Drivers 5.7 for PHP for SQL Server
 // Copyright(c) Microsoft Corporation
 // All rights reserved.
 // MIT License
--- source/shared/core_util.cpp
+++ source/shared/core_util.cpp
@@ -5,7 +5,7 @@
 // 
 // Comments: Mostly error handling and some type handling
 //
-// Microsoft Drivers 5.6 for PHP for SQL Server
+// Microsoft Drivers 5.7 for PHP for SQL Server
 // Copyright(c) Microsoft Corporation
 // All rights reserved.
 // MIT License
