39806acab98c39fd765a7f0f605a0964e17ea9ed
George Carrette <gcarrette@wayfair.com>
2021-10-05 17:13:48 -0400

    Be more explicit, only deal with work around for direction SQL_PARAM_INPUT

--- source/shared/core_stmt.cpp
+++ source/shared/core_stmt.cpp
@@ -2496,7 +2496,7 @@ void sqlsrv_param::bind_param(_Inout_ sqlsrv_stmt* stmt)
 {
     if (was_null) {
         strlen_or_indptr = SQL_NULL_DATA;
-	if ((sql_data_type == SQL_CHAR) && (column_size == 1)) {
+	if ((sql_data_type == SQL_CHAR) && (column_size == 1) && (direction == SQL_PARAM_INPUT)) {
 	  // This is a work-around for a bug in Microsoft SQL Server and/or unixODBC and/or msodbcsql;
 	  // as to how the T-SQL operator ISNULL likes to see a NULL.
 	  sql_data_type = SQL_VARCHAR;
