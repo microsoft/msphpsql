cbf008fac1787422b9e6f32280583816eb3822d2
Samuel Štancl <samuel@archte.ch>
2026-02-02 06:48:28 +0100

    PHP 8.5 (compilation) support (#1543)
    Since https://github.com/php/php-src/pull/17741, dbh->error_mode is
    stored as uint8_t. We cast the value to pdo_error_mode for the
    assignment to work.
    
    Since https://github.com/php/php-src/pull/17742, query_stmt_zval has
    been replaced by query_stmt_obj. The diff in the linked PR shows
    the new destructor usage. We follow that same usage in this commit.
    
    Co-authored-by: Jahnvi Thakkar <61936179+jahnvi480@users.noreply.github.com>

--- source/pdo_sqlsrv/pdo_dbh.cpp
+++ source/pdo_sqlsrv/pdo_dbh.cpp
@@ -625,7 +625,7 @@ int pdo_sqlsrv_db_handle_factory( _Inout_ pdo_dbh_t *dbh, _In_opt_ zval *driver_
     PDO_LOG_DBH_ENTRY;
 
     hash_auto_ptr pdo_conn_options_ht;
-    pdo_error_mode prev_err_mode = dbh->error_mode;
+    pdo_error_mode prev_err_mode = static_cast<pdo_error_mode>( dbh->error_mode );
 
     // must be done in all cases so that even a failed connection can query the
     // object for errors.
@@ -1604,7 +1604,7 @@ zend_string * pdo_sqlsrv_dbh_last_id(_Inout_ pdo_dbh_t *dbh, _In_ const zend_str
     PDO_LOG_DBH_ENTRY;
 
     // turn off any error handling for last_id
-    pdo_error_mode prev_err_mode = dbh->error_mode;
+    pdo_error_mode prev_err_mode = static_cast<pdo_error_mode>( dbh->error_mode );
     dbh->error_mode = PDO_ERRMODE_SILENT;
 
     sqlsrv_malloc_auto_ptr<sqlsrv_stmt> driver_stmt;
