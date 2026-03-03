4636b8bce6893d4d0ee02b401d3681f220ccf39b
David Engel <davidengel@microsoft.com>
2026-03-03 18:01:12 +0000

    Fix connection options for SQLCMD and BCP in setup_dbs.py

--- test/functional/setup/setup_dbs.py
+++ test/functional/setup/setup_dbs.py
@@ -25,28 +25,28 @@ def _is_mssqltools_v18():
 # optional + trust-server-certificate so sqlcmd/bcp work against servers
 # without a valid TLS certificate. Empty strings for older tools.
 _v18 = _is_mssqltools_v18()
-_encrypt_opt_sqlcmd = ' -Yo -C' if _v18 else ''
+_encrypt_opt_sqlcmd = ' -No -C' if _v18 else ''
 _encrypt_opt_bcp   = ' -Yo -u' if _v18 else ''
 
-def setupTestDatabase(conn_options, dbname, azure):
+def setupTestDatabase(conn_options_sqlcmd, dbname, azure):
     sqlFiles = ['test_types.sql', '168256.sql', 'cd_info.sql', 'tracks.sql']
 
     for sqlFile in sqlFiles:
-        executeSQLscript(sqlFile, conn_options, dbname)
+        executeSQLscript(sqlFile, conn_options_sqlcmd, dbname)
 
-def populateTables(conn_options, dbname):
-    executeBulkCopy(conn_options, dbname, 'cd_info', 'cd_info')
-    executeBulkCopy(conn_options, dbname, 'tracks', 'tracks')
-    executeBulkCopy(conn_options, dbname, 'test_streamable_types', 'test_streamable_types')
-    executeBulkCopy(conn_options, dbname, '159137', 'xml')
-    executeBulkCopy(conn_options, dbname, '168256', '168256')
+def populateTables(conn_options_bcp, dbname):
+    executeBulkCopy(conn_options_bcp, dbname, 'cd_info', 'cd_info')
+    executeBulkCopy(conn_options_bcp, dbname, 'tracks', 'tracks')
+    executeBulkCopy(conn_options_bcp, dbname, 'test_streamable_types', 'test_streamable_types')
+    executeBulkCopy(conn_options_bcp, dbname, '159137', 'xml')
+    executeBulkCopy(conn_options_bcp, dbname, '168256', '168256')
 
-def executeBulkCopy(conn_options, dbname, tblname, datafile):
-    redirect_string = 'bcp {0}..{1} in {2}.dat -f {2}.fmt -q' + _encrypt_opt_bcp
-    inst_command = redirect_string.format(dbname, tblname, datafile) + conn_options
+def executeBulkCopy(conn_options_bcp, dbname, tblname, datafile):
+    redirect_string = 'bcp {0}..{1} in {2}.dat -f {2}.fmt -q'
+    inst_command = redirect_string.format(dbname, tblname, datafile) + conn_options_bcp
     executeCommmand(inst_command)
 
-def setupAE(conn_options, dbname):
+def setupAE(conn_options_sqlcmd, dbname):
     if (platform.system() == 'Windows'):
         # import self signed certificate
         inst_command = "certutil -user -p '' -importPFX My PHPcert.pfx NoRoot"
@@ -54,7 +54,7 @@ def setupAE(conn_options, dbname):
         inst_command = "certutil -user -p '' -importPFX My AEV2Cert.pfx NoRoot"
         executeCommmand(inst_command)
         # create Column Master Key and Column Encryption Key
-        script_command = 'sqlcmd -I ' + conn_options + ' -i ae_keys.sql -d ' + dbname
+        script_command = 'sqlcmd -I ' + conn_options_sqlcmd + ' -i ae_keys.sql -d ' + dbname
         executeCommmand(script_command)
 
 if __name__ == '__main__':
@@ -77,20 +77,22 @@ if __name__ == '__main__':
 
     current_working_dir=os.getcwd()
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
-    conn_options = ' -S ' + server + ' -U ' + uid + ' -P ' + pwd + _encrypt_opt_sqlcmd + ' '
+    base_conn = ' -S ' + server + ' -U ' + uid + ' -P ' + pwd + ' '
+    conn_options_sqlcmd = base_conn + _encrypt_opt_sqlcmd
+    conn_options_bcp = base_conn + _encrypt_opt_bcp
 
     # In Azure, assume an empty test database has been created using Azure portal
     if (args.AZURE.lower() == 'no'):
-        manageTestDB('create_db.sql', conn_options, args.DBNAME)
+        manageTestDB('create_db.sql', conn_options_sqlcmd, args.DBNAME)
 
     print("About to set up databases...\n")
     # create tables in the new database
-    setupTestDatabase(conn_options, args.DBNAME, args.AZURE)
+    setupTestDatabase(conn_options_sqlcmd, args.DBNAME, args.AZURE)
     print("About to populate tables...\n")
     # populate these tables
-    populateTables(conn_options, args.DBNAME)
+    populateTables(conn_options_bcp, args.DBNAME)
     print("About to set up encryption...\n")
     # setup AE (certificate, column master key and column encryption key)
-    setupAE(conn_options, args.DBNAME)
+    setupAE(conn_options_sqlcmd, args.DBNAME)
 
     os.chdir(current_working_dir)
